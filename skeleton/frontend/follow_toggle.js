6e9444c65cb574afc5e6715fe7e0c9c888d9e572
Jonathon Wright <jonwright93@gmail.com>
2017-03-01 11:10:28 -0800

    add FollowToggle class

---
+++ skeleton/frontend/follow_toggle.js
@@ -0,0 +1,11 @@
+class FollowToggle{
+  constructor(el) {
+    this.$el = $("el");
+    this.userId = this.$el.find("user-id");
+    this.initialFollowState = this.$el.find("initial-follow-state");
+  }
+}
+
+
+
+module.exports = FollowToggle;
